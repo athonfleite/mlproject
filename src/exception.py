64dc61310e593173c0f57a31f2700790d4606d9f
athonfleite <athon_f@yahoo.com.br>
2023-09-25 20:07:46 -0300

    Updtaed exception and logger

--- src/exception.py
+++ src/exception.py
@@ -1,4 +1,6 @@
 import sys
+import logging
+from src.logger import logging
 
 def error_message_detail(error, error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
@@ -9,7 +11,7 @@ def error_message_detail(error, error_detail:sys):
 
 class CustomException(Exception):
     def __init__(self, error_message, error_detail:sys):
-        super.__init__(error_message)
+        super().__init__(error_message)
         self.error_message = error_message_detail(error_message, error_detail)
 
     def __str__(self):
