88e6f258c7e547d54662289ebb498371caf2de72
athonfleite <athon_f@yahoo.com.br>
2024-02-05 20:38:34 -0300

    Finished Data ingestion class with csv example

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -22,10 +22,19 @@ class DataIngestion:
     def start_data_ingestion(self):
         logging.info("Entered data ingestion component")
         try:
+            #Read from CSV, change to other sources
             df = pd.read_csv('notebook\data\stud.csv')
             logging.info("Converted to dataframe")
-            
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
-            df.to_csv(self.ingestion_config.train_data_path)
-        except:
-            pass
+            df.to_csv(self.ingestion_config.raw_data_path)
+            logging.info("Train test split initiated")
+            train_set,test_set = train_test_split(df,test_size=0.2, random_state=42)
+            #Save CSVs
+            train_set.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
+            test_set.to_csv(self.ingestion_config.test_data_path, index=False, header=True)
+            logging.info("Data Ingestion completed")
+
+            return (self.ingestion_config.train_data_path, self.ingestion_config.test_data_path)
+        except Exception as e:
+            raise CustomException(e,sys)
+        
