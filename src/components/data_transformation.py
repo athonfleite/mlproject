892ad198ce83fe701746a481df71ab7f9453d77d
athonfleite <athon_f@yahoo.com.br>
2024-02-06 20:04:30 -0300

    Initial implementation of DataTransformation class

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -16,3 +16,41 @@ from src.logger import logging
 class DataTransformationConfig:
     preprocess_obj_file_path = os.path.join('artifact', 'preprocessor.pkl')
 
+class DataTransformation:
+    def __init__(self):
+        self.data_transformation_config = DataTransformationConfig()
+
+    def get_data_transformer_object(self):
+        try:
+            numerical_columns = ["writing_score", "reading_score"]
+            categorical_columns = [
+                                   "gender",
+                                   "race_ethnicity",
+                                   "parental_level_of_education",
+                                   "lunch",
+                                   "test_preparation_course"
+                                   ]
+            num_pipeline = Pipeline(
+                steps = [
+                    ("imputer", SimpleImputer(strategy="median")),
+                    ("scaler", StandardScaler())
+                ]
+            )
+
+            logging.info("Numerical columns scaling completed")
+            cat_pipeline = Pipeline(
+                steps = [
+                    ("imputer", SimpleImputer(strategy = "most_frequent")),
+                    ("one_hot_encoder", OneHotEncoder()),
+                    ("scaler", StandardScaler())
+                ]
+            )
+            logging.info("Categorical columns encoding completed")
+
+            preprocessor = ColumnTransformer(
+                [
+                    ("num_pipeline", num_pipeline, numerical_columns)
+                ]
+            )
+        except:
+            pass
